4753c15983ac251819b5d2a4087b0c651d63d906
Max Jacobson <max@hardscrabble.net>
2015-07-14 22:35:55 -0400

    update to appease rubocop

--- lib/rubo_cop/single_responsibility_principle.rb
+++ lib/rubo_cop/single_responsibility_principle.rb
@@ -1,21 +1,25 @@
+require "rubocop"
 require "rubo_cop/single_responsibility_principle/version"
 
 module RuboCop
   module Cop
     module Style
+      # Require that classes implement exactly one method
       class OnlyOneMethodPerClass < Cop
         def investigate(processed_source)
           ast = processed_source.ast
           return unless ast
 
           ast.each_node(:class) do |node|
-            _, _, body = *node
             # TODO: consider attr_accessors as well
             # TODO: distinguish between public and private
-            if (count = body.each_child_node.count(&:def_type?)) > 1
-              # TODO: what should the second thing be...?
-              add_offense(node, :expression, "Detected #{count} methods; only one is allowed.")
-            end
+            _, _, body = *node
+            count = body.each_child_node.count(&:def_type?)
+            next if count == 1
+
+            # TODO: what should the second thing be...?
+            msg = "Detected #{count} methods; only one is allowed."
+            add_offense(node, :expression, msg)
           end
         end
       end
