fcb8499537332b0bec99a9af7e4bd760a77b8c75
Max Jacobson <max@hardscrabble.net>
2015-07-14 21:41:24 -0400

    Setup some more
    * describe gem in gemspec
    * add rubocop (with some light configuration)
    * add some binstubs

--- spec/rubo_cop/single_responsibility_principle_spec.rb
+++ spec/rubo_cop/single_responsibility_principle_spec.rb
@@ -4,8 +4,4 @@ describe RuboCop::SingleResponsibilityPrinciple do
   it 'has a version number' do
     expect(RuboCop::SingleResponsibilityPrinciple::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
